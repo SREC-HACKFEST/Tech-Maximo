afa7eef78632e15f6b3cc7e352a7de50d0b38efe
Vijay Sam <vijay203.vk@gmail.com>
2022-04-07 21:52:06 +0530

    Added superuser

--- scs/cbcs/urls.py
+++ scs/cbcs/urls.py
@@ -1,5 +1,7 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.student, name="student")
+    path('', views.student, name="student"),
+    path('courses/', views.course, name="course")
 ]
--- scs/cbcs/views.py
+++ scs/cbcs/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def student(request):
     return render(request, 'profile.html')
+
+
+def course(request):
+    return render(request, 'base.html')
